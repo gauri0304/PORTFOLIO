8eadf64cb5a0814e7c68d186e9ad8e393b11565d
gauri0304 <gaurimhatre2001@gmail.com>
2025-06-23 19:42:07 +0530

    Updated files

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 $(function () {
 
+    // Menu toggle
     $(".menu-bars").on("click", function () {
-        if ($(".menu-responsive").is(":visible")) {
-            $(".menu-responsive").hide(); // Instantly hides
-        } else {
-            $(".menu-responsive").show(); // Instantly shows
+        $(".menu-responsive").toggle(); // Simple toggle
+    });
+
+    // Smooth scroll
+    $(".scroll").on("click", function (e) {
+        e.preventDefault();
+        const target = this.hash;
+        const element = document.querySelector(target);
+        if (element) {
+            element.scrollIntoView({
+                behavior: "smooth",
+                block: "start"
+            });
         }
     });
 
-    // Scroll click on menu
-    $(".scroll").on("click", function () {
-        $(this).each(function () {
-            const sectionTop = $(this.hash).offset().top;
-            $("html, body").animate({
-                scrollTop: sectionTop
-            }, 1500);
-        });
-    });
-
-    // Progress Bar Animate
+    // ScrollReveal Progress Bars
     window.sr = ScrollReveal();
     sr.reveal(".progress-bar", {
         origin: "left",
@@ -26,20 +26,16 @@ $(function () {
         distance: "100%"
     });
 
-
-    // Contact Text Animate
+    // Text Animation
     const sentences = ["web designer ?", "web developer ?"];
     let i = 0;
-
     setInterval(function () {
         $(".text-animate").fadeOut(function () {
             $(this).text(sentences[i = (i + 1) % sentences.length]).fadeIn();
         });
     }, 2500);
 
-});
-
-$(document).ready(function () {
+    // Project cards (card-medium toggle)
     $(".card-medium").each(function () {
         const $card = $(this);
         const $infoIcon = $card.find(".fa-circle-info");
@@ -47,15 +43,12 @@ $(document).ready(function () {
         const $content = $card.find(".card-content");
         const $githubLink = $card.find(".card-git-content a");
 
-        // Initially hide the content
         $content.removeClass("expanded");
 
-        // Toggle on info icon click
         $infoIcon.on("click", function () {
             $content.toggleClass("expanded");
         });
 
-        // Open GitHub on GitHub icon click
         $githubIcon.on("click", function () {
             const url = $githubLink.attr("href");
             if (url) {
@@ -63,24 +56,30 @@ $(document).ready(function () {
             }
         });
     });
+
+    // New: Project-card image click toggle
+    $(".project-image").on("click", function () {
+        const $content = $(this).siblings(".project-content");
+        $content.toggleClass("show");
+    });
 });
 
-
+// Back to Top Button
 let mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+    scrollFunction();
+};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    document.body.scrollTop = 0; // Safari
+    document.documentElement.scrollTop = 0; // Chrome, Firefox, IE, Opera
 }
